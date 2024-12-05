961ea92fd7a97fbe8d5cb9ec91e9d30fd57a2e19
LeonSnajdr <leonsnajdr@gmail.com>
2024-12-05 15:25:58 +0100

    Add editor bones

--- packages/examples/vuetify/src/main.ts
+++ packages/examples/vuetify/src/main.ts
@@ -1,10 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import { createVuetify } from "vuetify";
+import vueTextInsert from "vue-text-insert";
 
 const app = createApp(App);
 const vuetify = createVuetify();
 
 app.use(vuetify);
+app.use(vueTextInsert);
 
 app.mount("#app");
--- packages/vue-text-insert/src/components/VueTextInsert.vue
+++ packages/vue-text-insert/src/components/VueTextInsert.vue
@@ -1,3 +1,28 @@
 <template>
-    <h1>moingdd</h1>
+    <div ref="vueTextInsertEditor" id="vueTextInsertEditor" @input="input" @keydown.enter.prevent @copy.prevent="copy" @paste.prevent="paste" />
 </template>
+
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+
+const editor = ref<HTMLDivElement>();
+
+const input = () => {
+    throw new Error("Not implemented!");
+};
+
+const copy = () => {
+    throw new Error("Not implemented!");
+};
+
+const paste = () => {
+    throw new Error("Not implemented!");
+};
+</script>
+
+<style scoped>
+#vueTextInsertEditor {
+    width: 100%;
+    outline: none;
+}
+</style>
